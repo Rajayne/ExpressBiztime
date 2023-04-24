658fa735654cb0d66d532c9f242c911ac9e17659
Rajayne <ryann.j.nealey@gmail.com>
2023-04-24 11:07:06 -0700

    Create post companies route

--- routes/companies.js
+++ routes/companies.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db");
 const ExpressError = require("../expressError");
+const { request } = require("../app");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -23,6 +24,19 @@ router.get("/:code", async (req, res, next) => {
       throw new ExpressError(`No company found with code of ${code}`, 404);
     }
     return res.json({ company: companyByCode.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const { code, name, description } = req.body;
+    const newCompany = await db.query(
+      `INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING *`,
+      [code, name, description]
+    );
+    return res.status(201).json({ company: newCompany.rows[0] });
   } catch (e) {
     return next(e);
   }
