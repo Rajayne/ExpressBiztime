f77b5a8bcd6f3a3d7c9e684e649171bca82f7dfb
Rajayne <ryann.j.nealey@gmail.com>
2023-04-24 10:34:31 -0700

    Load seed data and configure db.js to connect to database

--- db.js
+++ db.js
@@ -1,2 +1,15 @@
-/** Database setup for BizTime. */
+const { Client } = require("pg");
+const { user, password } = require(".../dbPassword");
 
+let DB_URI =
+  process.env.NODE_ENV === "test"
+    ? `postgresql://${user}:${password}@localhost:5432/biztime_test`
+    : `postgresql://${user}:${password}@localhost:5432/biztime`;
+
+let db = new Client({
+  connectionString: DB_URI,
+});
+
+db.connect();
+
+module.exports = db;
