237b19603f0d0da8f806e6d8aa003daa7e926c5d
Rajayne <ryann.j.nealey@gmail.com>
2023-04-24 12:22:53 -0700

    Edit get invoice by ID to include company info

--- routes/invoices.js
+++ routes/invoices.js
@@ -16,11 +16,37 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
-    const invoice = await db.query(`SELECT * FROM invoices WHERE id=$1`, [id]);
-    if (invoice.rows.length === 0) {
+    const result = await db.query(
+      `SELECT i.id, 
+                i.comp_code, 
+                i.amt, 
+                i.paid, 
+                i.add_date, 
+                i.paid_date, 
+                c.name, 
+                c.description 
+         FROM invoices AS i
+           INNER JOIN companies AS c ON (i.comp_code = c.code)  
+         WHERE id = $1`,
+      [id]
+    );
+    if (result.rows.length === 0) {
       throw new ExpressError(`No invoice found with id of ${id}`, 404);
     }
-    return res.json({ invoice: invoice.rows });
+    const data = result.rows[0];
+    const invoice = {
+      id: data.id,
+      company: {
+        code: data.comp_code,
+        name: data.name,
+        description: data.description,
+      },
+      amt: data.amt,
+      paid: data.paid,
+      add_date: data.add_date,
+      paid_date: data.paid_date,
+    };
+    return res.json({ invoice: invoice });
   } catch (e) {
     return next(e);
   }
