7e5a15c70e49e2a14031149155d82e1ba45272bf
Rajayne <ryann.j.nealey@gmail.com>
2023-04-24 10:57:40 -0700

    Get route by company code with errors

--- routes/companies.js
+++ routes/companies.js
@@ -12,4 +12,20 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/:code", async (req, res, next) => {
+  try {
+    const { code } = req.params;
+    const companyByCode = await db.query(
+      `SELECT * FROM companies WHERE code=$1`,
+      [code]
+    );
+    if (companyByCode.rows.length === 0) {
+      throw new ExpressError(`No company found with code of ${code}`, 404);
+    }
+    return res.json({ company: companyByCode.rows[0] });
+  } catch (e) {
+    return next(e);
+  }
+});
+
 module.exports = router;
