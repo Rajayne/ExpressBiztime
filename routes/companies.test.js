b3ae0cd899a5975d9de422ee51b3d3ea2aeb227d
Rajayne <ryann.j.nealey@gmail.com>
2023-04-26 09:30:07 -0700

    Create Get company by code test

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -4,20 +4,14 @@ const app = require("../app");
 process.env.NODE_ENV = "test";
 const db = require("../db");
 
-let testCompany = {
-  code: "google",
-  name: "Google",
-  description: "Internet search engine.",
-};
+let testCompany;
 
 // Create new company in db before each test
 beforeEach(async () => {
-  const { code, name, description } = testCompany;
   const test = await db.query(
-    `INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING *`,
-    [code, name, description]
+    `INSERT INTO companies (code, name, description) VALUES ('google', 'Google', 'Internet search engine.') RETURNING *`
   );
-  testUser = test.rows[0];
+  testCompany = test.rows[0];
 });
 
 afterEach(async () => {
@@ -30,7 +24,6 @@ afterAll(async () => {
 
 describe("Test beforeEach", () => {
   test("Create testCompany", () => {
-    console.log(testCompany);
     expect(1).toBe(1);
   });
 });
@@ -39,9 +32,20 @@ describe("GET /companies", () => {
   test("Get list of all companies", async () => {
     const result = await request(app).get("/companies");
     expect(result.statusCode).toBe(200);
-    console.log(result.rows);
     expect(result.body).toEqual({
       companies: [{ code: testCompany.code, name: testCompany.name }],
+    });
+  });
+});
+
+describe("GET /companies/:code", () => {
+  test("Get a single company", async () => {
+    const result = await request(app).get(`/companies/${testCompany.code}`);
+    expect(result.statusCode).toBe(200);
+    // Set testCompany invoices to empty array
+    testCompany.invoices = [];
+    expect(result.body).toEqual({
+      company: testCompany,
     });
   });
 });
